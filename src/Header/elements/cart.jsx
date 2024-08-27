09b4cdba562bbc3d2730af9f8bb6720062dd53bb
govinda <govinda@gmail.com>
2024-08-27 18:47:11 +0530

    change

--- src/Header/elements/cart.jsx
+++ src/Header/elements/cart.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../elements/cart.css";
 import { useSelector } from "react-redux";
-import {removeItem} from "../../redux/slices/cartSlice"
+import { removeItem } from "../../redux/slices/cartSlice";
 import { useDispatch } from "react-redux";
 
 const Cart = () => {
   const items = useSelector((state) => state?.cart || []);
-  const dispatch = useDispatch()
-  
+  const dispatch = useDispatch();
+
   // Ensure prices are numbers and calculate the total
   const total = items.reduce((acc, item) => acc + Number(item.price), 0);
 
@@ -19,9 +19,8 @@ const Cart = () => {
             <h1 id="cart-title">Your Cart</h1>
             <table id="cart-table">
               <thead>
-                <tr className="table-row">\
-                
-                  <th className="table-header">Item</th>
+                <tr className="table-row">
+                  \<th className="table-header">Item</th>
                   {/* <th className="table-header"><pre>       </pre></th> */}
                   <th className="table-header">Price</th>
                   <th className="table-header">Remove</th>
@@ -31,17 +30,24 @@ const Cart = () => {
                 {items.map((e, index) => (
                   <tr key={index}>
                     <td>{e.id}</td>
-                    <td className="itemsName">{e.name.substring(0 , 13)}</td>
+                    <td className="itemsName">{e.name.substring(0, 13)}</td>
                     {/* <td><pre>      </pre></td> */}
                     <td>{e.price}</td>
-                    <td><button className="remove-btn" onClick={(e)=>dispatch(removeItem(index))}><i class="fa-solid fa-xmark"></i></button></td>
+                    <td>
+                      <button
+                        className="remove-btn"
+                        onClick={(e) => dispatch(removeItem(index))}
+                      >
+                        <i class="fa-solid fa-xmark"></i>
+                      </button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
             </table>
             <div id="cart-summary">
               <p id="total-label">Total:</p>
-              <p id="total-amount">Rs. {total}</p>
+              <p id="total-amount">Rs. {total} /-</p>
             </div>
             <button id="checkout-button">Proceed to Checkout</button>
           </div>
